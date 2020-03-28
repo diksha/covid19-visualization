7133764c34dd49f02cbcb16966b126e5c2278979
Lisa Dmello <lisadmello@fb.com>
2020-03-28 14:32:58 -0700

    Add func to read CSV into array

--- populate_markers.js
+++ populate_markers.js
@@ -40,3 +40,16 @@ function getColorForResourceValue(resourceScore) {
 function computeScoreForHospital(beds, masks, ventilators, kits) {
 	return beds + masks + ventilators + kits;
 }
+
+function parseSuppliesCSVIntoArray() {
+	var url = "https://raw.githubusercontent.com/diksha/covid19-visualization/master/supplies.csv";
+	var request = new XMLHttpRequest();
+	request.open("GET", url, false);
+	request.send(null);
+	var csvData = new Array();
+	var jsonObject = request.responseText.split(/\r?\n|\r/);
+	for (var i = 0; i < jsonObject.length; i++) {
+	  csvData.push(jsonObject[i].split(','));
+	}
+	return csvData;
+}
