5ca6664999b286f322ad071a248612273e363d90
Diksha Gohlyan <gr8bigdg@gmail.com>
2020-03-28 11:42:08 -0700

    Add users current geolocation to the maps

--- init_map.js
+++ init_map.js
@@ -1,7 +1,23 @@
+var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 37.3688, lng: -122.0363},
 		zoom: 8
 	});
+	getGeoLocation();
 	populateMarkers(map);
+}
+
+function getGeoLocation() {
+// Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+      }, function() {
+      });
+   }
 }
