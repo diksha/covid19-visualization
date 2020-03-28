1e9fc833877ceb7d4718fa003df5e74f0ffbe55c
Lisa Dmello <lisadmello@fb.com>
2020-03-28 15:17:23 -0700

    Read CSV correctly + bug fix

--- populate_markers.js
+++ populate_markers.js
@@ -1,32 +1,19 @@
 function populateMarkers(map) {
-
-	var supplies = [
-	{ placeId:"ChIJxYu-ZtK2j4ARpLis1OcKNDM", beds:"-37", masks:"35", ventilators:"27", kits:"-25"},
-	{ placeId:"ChIJ_3X2kATNj4ARJq22N-TR2io", beds:"0", masks:"-5", ventilators:"15", kits:"29"},
-	{ placeId:"ChIJn43dqhnLj4ARaHaMv8hRGmY", beds:"-41", masks:"-48", ventilators:"34", kits:"-7"},
-	{ placeId:"ChIJvyb4WqXHwoAR91h7k6g_HXY", beds:"26", masks:"-50", ventilators:"-6", kits:"22"},
-	{ placeId:"ChIJM9J-ccbHwoARlaXxTFx9ck4", beds:"-27", masks:"45", ventilators:"41", kits:"-47"},
-	{ placeId:"ChIJN9byrje_woARvQL9tPmsa10", beds:"-48", masks:"4", ventilators:"44", kits:"-12"},
-	{ placeId:"ChIJx3kMjY8NkFQR9nFRWgZ3Ypc", beds:"-29", masks:"-8", ventilators:"-48", kits:"-28"},
-	{ placeId:"ChIJGUt2ibVqkFQRAB3xp3UjRLw", beds:"-6", masks:"0", ventilators:"-27", kits:"-27"},
-	{ placeId:"ChIJrYx10YIUkFQRXOPoZdRuFaA", beds:"-28", masks:"-4", ventilators:"-21", kits:"-48"},
-	{ placeId:"ChIJ61x_cpz2wokRG1SmA_ez-kM", beds:"34", masks:"6", ventilators:"14", kits:"-32"},
-	{ placeId:"ChIJqccvIQxZwokR4PfF_NqTomc", beds:"50", masks:"36", ventilators:"-38", kits:"-17"},
-	{ placeId:"ChIJra13dJVYwokRh3kQ5-p7Nio", beds:"22", masks:"21", ventilators:"44", kits:"-8"},
-	{ placeId:"ChIJ69Amvc0cdkgRLNOj6dq9K9k", beds:"33", masks:"17", ventilators:"-20", kits:"9"},
-	{ placeId:"ChIJe4cW3cAEdkgR0RK8zGZJPAQ", beds:"39", masks:"35", ventilators:"1", kits:"9"},
-	{ placeId:"ChIJE52681ADdkgR6Sv8TJwvcXU", beds:"-47", masks:"-26", ventilators:"30", kits:"-9"}
-	]
-
+	supplies = parseSuppliesCSVIntoArray();
 	infowindow = new google.maps.InfoWindow();
 	service = new google.maps.places.PlacesService(map);
 	supplies.forEach(iterate);
 }
 
 function iterate(supply) {
-	service.getDetails(getPlaceRequest(supply['placeId']), function(place, status) {
+	var placeID = supply[0];
+	service.getDetails(getPlaceRequest(placeID), function(place, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
-				var score = computeScoreForHospital(supply['beds'], supply['masks'], supply['ventilators'], supply['kits']);
+				var beds = supply[1];
+				var masks = supply[2];
+				var ventilators = supply[3];
+				var kits = supply[4];
+				var score = computeScoreForHospital(beds, masks, ventilators, kits);
 				var color = getColorForResourceValue(score);
 				// need to figure out how to draw a cross shape
 				var hospitalStar = {
@@ -34,7 +21,6 @@ function iterate(supply) {
           fillColor: color,
           fillOpacity: 0.8,
           scale: 0.1,
-          strokeWeight: 1
 	       };
         var marker = new google.maps.Marker({
           map: map,
@@ -83,7 +69,7 @@ function getColorForResourceValue(resourceScore) {
 }
 
 function computeScoreForHospital(beds, masks, ventilators, kits) {
-	return beds + masks + ventilators + kits;
+	return parseInt(beds) + parseInt(masks) + parseInt(ventilators) + parseInt(kits);
 }
 
 function parseSuppliesCSVIntoArray() {
