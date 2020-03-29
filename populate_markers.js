b22201164c779d0419820bfc9dce37aec0bd43ba
Diksha Gohlyan <gr8bigdg@gmail.com>
2020-03-28 18:08:59 -0700

    When only 1 selected, sow value in marker.

--- populate_markers.js
+++ populate_markers.js
@@ -2,6 +2,7 @@ var masksSelected ;
 var ventilatorsSelected ;
 var bedsSelected ;
 var kitsSelected ;
+var markerArray;
 function populateMarkers(map) {
 	infowindow = new google.maps.InfoWindow();
 	service = new google.maps.places.PlacesService(map);
@@ -9,15 +10,16 @@ function populateMarkers(map) {
 }
 
 function recalculate() {
-	masksSelected = document.getElementById('masks').checked;
-	ventilatorsSelected = document.getElementById('ventilators').checked;
-	bedsSelected  = document.getElementById('beds').checked;
-	kitsSelected  = document.getElementById('kits').checked;
+	masksSelected = document.getElementById('masks').checked ? 1 : 0;
+	ventilatorsSelected = document.getElementById('ventilators').checked ? 1 : 0;
+	bedsSelected  = document.getElementById('beds').checked ? 1 : 0;
+	kitsSelected  = document.getElementById('kits').checked ? 1 : 0;
 	supplies = parseSuppliesCSVIntoArray();
 	supplies.forEach(iterate);
 }
 
 function iterate(supply) {
+	clearAllMarkers();
 	var placeID = supply[0];
 	service.getDetails(getPlaceRequest(placeID), function(place, status) {
 		if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -27,19 +29,45 @@ function iterate(supply) {
 			var kits = supply[4];
 			var score = computeScoreForHospital(beds, masks, ventilators, kits);
 			var color = getColorForResourceValue(score);
-			var hospitalStar = {
-				 path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-				 fillColor: color,
-				 fillOpacity: 1,
-				 scale: 0.1,
-				 strokeWeight: 1,
-			};
-			var marker = new google.maps.Marker({
-				map: map,
-				position: place.geometry.location,
-				icon: hospitalStar,
-				title: "Hospital"
-			});
+			
+			var marker;
+			if(masksSelected+bedsSelected+kitsSelected+ventilatorsSelected == 1) {
+				var hospitalStar = {
+					path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+					fillColor: color,
+					fillOpacity: 1,
+					scale: 0.1,
+					strokeWeight: 1,
+					labelOrigin: new google.maps.Point(120, 120),
+				};
+				marker =  new google.maps.Marker({
+					map: map,
+					position: place.geometry.location,
+					icon: hospitalStar,
+					title: "Hospital",
+					label: {
+						text:(score).toString(),
+						fontSize:'6px',
+						fontWeight: 'bold',
+						color: 'white'
+					},
+				});
+			} else {
+				var hospitalStar = {
+					path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+					fillColor: color,
+					fillOpacity: 1,
+					scale: 0.1,
+					strokeWeight: 1,
+				};
+				marker =  new google.maps.Marker({
+					map: map,
+					position: place.geometry.location,
+					icon: hospitalStar,
+					title: "Hospital"
+				});
+			} 
+			markerArray.push(marker);
 			google.maps.event.addListener(marker, 'click', function() {
 				infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
 					'Address: ' + place.formatted_address + '<br>' +
@@ -53,6 +81,14 @@ function iterate(supply) {
 			});
 		};
 	});
+}
+
+function clearAllMarkers() {
+	if(!markerArray) markerArray = [];
+	for(i=0;i<markerArray.length;i++) {
+		markerArray[i].setMap(null);
+	}
+	markerArray = [];
 }
 
 function getPlaceRequest(pid) {
@@ -83,7 +119,6 @@ function getColorForResourceValue(resourceScore) {
 function computeScoreForHospital(beds, masks, ventilators, kits) {
 	return bedsSelected * parseInt(beds) + masksSelected*parseInt(masks) + ventilatorsSelected*parseInt(ventilators) + kitsSelected*parseInt(kits);
 }
-
 function parseSuppliesCSVIntoArray() {
 	var url = "https://raw.githubusercontent.com/diksha/covid19-visualization/master/supplies.csv";
 	var request = new XMLHttpRequest();
