0e409d9705b908a919fc37c7fca0004b1b4b85ba
Diksha Gohlyan <gr8bigdg@gmail.com>
2020-03-29 10:30:33 -0700

    Refactor to populate place infromation only once

--- populate_markers.js
+++ populate_markers.js
@@ -3,10 +3,44 @@ var ventilatorsSelected ;
 var bedsSelected ;
 var kitsSelected ;
 var markerArray;
+var placeInformationArray;
 function populateMarkers(map) {
 	infowindow = new google.maps.InfoWindow();
 	service = new google.maps.places.PlacesService(map);
-	recalculate();
+	getPlaceInformation(service);
+}
+
+function getPlaceInformation(service) {
+	placeInformationArray = new Array();
+	supplies = parseSuppliesCSVIntoArray();
+	promises = new Array();
+	for(i=0;i<supplies.length;i++) {
+		promises.push(getPlaceInfo(supplies[i], service));
+
+	}
+	Promise.all(promises).then(() => { 
+		recalculate();
+	});
+}
+
+function getPlaceInfo(supply, service) {
+	return new Promise(function(resolve,refuse) {
+		var placeInformation = new Array();
+		var placeID = supply[0];
+		for(j=0;j<supply.length;j++) {
+			placeInformation.push(supply[j]);
+		}
+		service.getDetails(getPlaceRequest(placeID), function(place, status) {
+			if (status === google.maps.places.PlacesServiceStatus.OK) {
+				placeInformation.push(place.geometry.location);
+				placeInformation.push(place.name);
+				placeInformation.push(place.formatted_address);
+				placeInformation.push(place.formatted_phone_number);
+				placeInformationArray.push(placeInformation);
+			} 
+			resolve();
+		});
+	});
 }
 
 function recalculate() {
@@ -14,72 +48,66 @@ function recalculate() {
 	ventilatorsSelected = document.getElementById('ventilators').checked ? 1 : 0;
 	bedsSelected  = document.getElementById('beds').checked ? 1 : 0;
 	kitsSelected  = document.getElementById('kits').checked ? 1 : 0;
-	supplies = parseSuppliesCSVIntoArray();
-	supplies.forEach(iterate);
+	clearAllMarkers();
+	placeInformationArray.forEach(iterate);
 }
 
 function iterate(supply) {
-	clearAllMarkers();
 	var placeID = supply[0];
-	service.getDetails(getPlaceRequest(placeID), function(place, status) {
-		if (status === google.maps.places.PlacesServiceStatus.OK) {
-			var beds = supply[1];
-			var masks = supply[2];
-			var ventilators = supply[3];
-			var kits = supply[4];
-			var score = computeScoreForHospital(beds, masks, ventilators, kits);
-			var color = getColorForResourceValue(score);
-
-			var marker;
-			if(masksSelected+bedsSelected+kitsSelected+ventilatorsSelected == 1) {
-				var hospitalStar = {
-					path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-					fillColor: color,
-					fillOpacity: 1,
-					scale: 0.1,
-					strokeWeight: 1,
-					labelOrigin: new google.maps.Point(120, 120),
-				};
-				marker =  new google.maps.Marker({
-					map: map,
-					position: place.geometry.location,
-					icon: hospitalStar,
-					title: "Hospital",
-					label: {
-						text:(score).toString(),
-						fontSize:'6px',
-						fontWeight: 'bold',
-						color: 'white'
-					},
-				});
-			} else {
-				var hospitalStar = {
-					path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-					fillColor: color,
-					fillOpacity: 1,
-					scale: 0.1,
-					strokeWeight: 1,
-				};
-				marker =  new google.maps.Marker({
-					map: map,
-					position: place.geometry.location,
-					icon: hospitalStar,
-					title: "Hospital"
-				});
-			}
-			markerArray.push(marker);
-			google.maps.event.addListener(marker, 'click', function() {
-				infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-					'Address: ' + place.formatted_address + '<br>' +
-					'Phone: ' + place.formatted_phone_number + '<br>' +
-					'Beds: ' + beds + '<br>' +
-					'Masks: ' + masks + '<br>' +
-					'Ventilators: ' + ventilators + '<br>' +
-					'Kits: ' + kits + '<br>' +
-					'</div>');
-				infowindow.open(map, this);
-			});
+	var beds = supply[1];
+	var masks = supply[2];
+	var ventilators = supply[3];
+	var kits = supply[4];
+	var score = computeScoreForHospital(beds, masks, ventilators, kits);
+	var color = getColorForResourceValue(score);
+	var marker;
+	if(masksSelected+bedsSelected+kitsSelected+ventilatorsSelected == 1) {
+		var hospitalStar = {
+			path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+			fillColor: color,
+			fillOpacity: 1,
+			scale: 0.1,
+			strokeWeight: 1,
+			labelOrigin: new google.maps.Point(120, 120),
 		};
+		marker =  new google.maps.Marker({
+			map: map,
+			position: supply[5],
+			icon: hospitalStar,
+			title: "Hospital",
+			label: {
+				text:(score).toString(),
+				fontSize:'6px',
+				fontWeight: 'bold',
+				color: 'white'
+			},
+		});
+	} else {
+		var hospitalStar = {
+			path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+			fillColor: color,
+			fillOpacity: 1,
+			scale: 0.1,
+			strokeWeight: 1,
+		};
+		marker =  new google.maps.Marker({
+			map: map,
+			position: supply[5],
+			icon: hospitalStar,
+			title: "Hospital"
+		});
+	}
+	markerArray.push(marker);
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent('<div><strong>' + supply[6] + '</strong><br>' +
+			'Address: ' + supply[7] + '<br>' +
+			'Phone: ' + supply[8] + '<br>' +
+			'Beds: ' + beds + '<br>' +
+			'Masks: ' + masks + '<br>' +
+			'Ventilators: ' + ventilators + '<br>' +
+			'Kits: ' + kits + '<br>' +
+			'</div>');
+		infowindow.open(map, this);
 	});
 }
 
